e4035da647b11f6589e2f5cc78e193ebcd7446c3
launc <toshadrazdov@gmail.com>
2024-02-14 16:27:19 +0300

    modified tests/test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,16 +1,20 @@
 from gendiff_package.gendiff import generate_diff
 
 
+diff_1 = generate_diff('tests/fixtures/file1.json',
+                       'tests/fixtures/file2.json')
+diff_2 = generate_diff('tests/fixtures/file3.json',
+                       'tests/fixtures/file4.json')
+diff_empty = generate_diff('tests/fixtures/empty_a.json',
+                           'tests/fixtures/empty_b.json')
+
+
 def test_generate_diff():
-    diff1 = generate_diff('fixtures/file1.json', 'fixtures/file2.json')
-    diff2 = generate_diff('fixtures/file3.json', 'fixtures/file4.json')
+    with (open('tests/fixtures/output1.txt', 'r') as output1,
+          open('tests/fixtures/output2.txt', 'r') as output2):
+        assert diff_1 == output1.read(), 'test "1" has been failed!'
+        assert diff_2 == output2.read(), 'test "2" has been failed!'
 
-    with (open('fixtures/output1.txt', 'r') as output1,
-          open('fixtures/output2.txt', 'r') as output2):
-        assert diff1 == output1.read()
-        assert diff2 == output2.read()
-
-    diff_empty = generate_diff('fixtures/empty_a.json', 'fixtures/empty_b.json')
-
-    with open('fixtures/output_empty.txt', 'r') as empty:
-        assert diff_empty == empty.read()
+    with (open('tests/fixtures/output_empty.txt', 'r') as empty):
+        assert diff_empty == empty.read(), \
+            'comparison of empty "a" & "b" should return empty brackets!'
