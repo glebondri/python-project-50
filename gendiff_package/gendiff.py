6dd425abf752d21f79166a88fabb84785ed2c694
launc <toshadrazdov@gmail.com>
2024-02-15 17:23:30 +0300

    modified gendiff_package/gendiff.py

--- gendiff_package/gendiff.py
+++ gendiff_package/gendiff.py
@@ -1,8 +1,8 @@
+from gendiff_package.parser import parsed
 from gendiff_package.formation import formatted, formatted_diff
-from json import load
 
 
-def differ(a, b):
+def not_equal(a, b):
     return a != b
 
 
@@ -13,44 +13,29 @@ def has_equal_keys(a, b):
 def generate_diff(a, b):
     diff = []
 
-    with (open(a, 'r') as file_a,
-          open(b, 'r') as file_b):
-        json_a = sorted(load(file_a).items())
-        json_b = sorted(load(file_b).items())
+    file_a, file_b = (sorted(parsed(a).items()),
+                      sorted(parsed(b).items()))
 
-        index = 0
-        while True:
-            if index >= len(json_a):
-                if index < len(json_b):
-                    diff.append(formatted(data=json_b.pop(index),
-                                          sign='+'))
-
-                else:
-                    return formatted_diff(diff)
+    index = 0
+    while True:
+        if index >= len(file_a):
+            if index < len(file_b):
+                diff.append(formatted(data=file_b.pop(index),
+                                      sign='+'))
 
             else:
-                if differ(json_a[index], json_b[index]):
-                    diff.append(formatted(data=json_a[index],
-                                          sign='-'))
+                return formatted_diff(diff)
 
-                    if has_equal_keys(json_a[index], json_b[index]):
-                        diff.append(formatted(data=json_b.pop(index),
-                                              sign='+'))
-                    json_a.pop(index)
+        else:
+            if not_equal(file_a[index], file_b[index]):
+                diff.append(formatted(data=file_a[index],
+                                      sign='-'))
 
-                else:
-                    diff.append(formatted(data=json_a[index]))
-                    index += 1
+                if has_equal_keys(file_a[index], file_b[index]):
+                    diff.append(formatted(data=file_b.pop(index),
+                                          sign='+'))
+                file_a.pop(index)
 
-
-# if __name__ == '__main__':
-#     # there is a bug!
-#     # comparing smaller 'a' to larger 'b'
-#     # (which has key closer to the very beginning of alphabet...
-#     # ...than 'a' does), breaks the comparison algorithm
-#     # (e.g. in obsidian)
-#
-#     diff = generate_diff('../a.json', '../b.json')
-#     print(diff)
-#     # with open('../tests/fixtures/output.txt') as file:
-#     #     print(file.read())
+            else:
+                diff.append(formatted(data=file_a[index]))
+                index += 1
