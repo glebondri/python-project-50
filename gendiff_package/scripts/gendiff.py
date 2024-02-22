5ded6196531ec9b1c932fc3f20e763f4ebfa0f50
launc <toshadrazdov@gmail.com>
2024-02-22 21:56:06 +0300

    modified scripts/gendiff.py

--- gendiff_package/scripts/gendiff.py
+++ gendiff_package/scripts/gendiff.py
@@ -1,5 +1,6 @@
 import argparse
 from gendiff_package.gendiff import generate_diff
+from gendiff_package import formation
 
 
 def main():
@@ -8,12 +9,17 @@ def main():
         add_help=True
     )
 
-    parser.add_argument('-f', '--format', help='set format of output')
+    parser.add_argument('-f', '--format', help='set format of output',
+                        default='stylish')
+    # parser.add_argument('--debug', action='store_true')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
 
     arguments = parser.parse_args()
-    file_a, file_b = arguments.first_file, arguments.second_file
+    path_a, path_b = arguments.first_file, arguments.second_file
+    out_format = arguments.format
 
-    diff = generate_diff(file_a, file_b)
-    print(diff)
+    diff = generate_diff(path_a, path_b)
+
+    if out_format == 'stylish':
+        print(formation.format_stylish(diff))
