50a6d084ae07d51a2587b795cf825f8f3c2e31ad
launc <toshadrazdov@gmail.com>
2024-02-29 21:17:35 +0300

    modified gendiff.py

--- gendiff_package/gendiff.py
+++ gendiff_package/gendiff.py
@@ -1,13 +1,10 @@
 from gendiff_package.parser import parsed
-from gendiff_package.formation import stylish
-from gendiff_package.formation import plain
+from gendiff_package.formation import stylish, plain, jsonify
 
 
 def compare(a: dict, b: dict) -> list:
     if not a and not b:
         return []
-
-    diff = []
 
     common_sequence = sorted((a | b).items())
     keys_a, keys_b = (list(a.keys()),
@@ -15,12 +12,14 @@ def compare(a: dict, b: dict) -> list:
     values_a, values_b = (list(a.values()),
                           list(b.values()))
 
+    diff = []
+
     for line in common_sequence:
         key, value = line
 
         if line in a.items() and line in b.items():
             diff.append({'data': {'key': key,
-                                  'value': value}, 'sign': '~'})
+                                  'value': value}, 'state': 'unchanged'})
 
         elif key in keys_a and key in keys_b:
             index_a = keys_a.index(key)
@@ -32,20 +31,20 @@ def compare(a: dict, b: dict) -> list:
             if isinstance(value, dict):
                 diff.append({'data': {'key': key,
                                       'value': compare(value_a, value_b)},
-                             'sign': '~'})
+                             'state': 'unchanged'})
             else:
                 diff.append({'data': {'key': key,
                                       'value': {
                                           'old': value_a,
                                           'new': value_b}},
-                             'sign': '!'})  # aka. 'changed'
+                             'state': 'changed'})
 
         else:
             diff.append({'data': {'key': key,
                                   'value': value},
-                         'sign': ('+'
-                                  if key not in keys_a else
-                                  '-')})
+                         'state': ('added'
+                                   if key not in keys_a else
+                                   'removed')})
 
         # elif key not in keys_a:
         #     diff.append({'data': line,
@@ -59,9 +58,11 @@ def compare(a: dict, b: dict) -> list:
 
 
 def generate_diff(path_a: str, path_b: str, out_format: str) -> str:
-    """Looks for differences in both given files
+    """Looks for a differences relatively of original file to new
+    :arg path_a: Path to the original file
+    :arg path_b: Path to the new file
     :arg out_format: Format of the output ("stylish" or "plain")
-    :returns: Comparison result of both files in two possible formats"""
+    :returns: Comparison result in one of two possible formats"""
 
     data_a, data_b = (parsed(path_a),
                       parsed(path_b))
@@ -69,7 +70,8 @@ def generate_diff(path_a: str, path_b: str, out_format: str) -> str:
 
     alias = {
         'stylish': stylish.format,
-        'plain': plain.format
+        'plain': plain.format,
+        'json': jsonify.format
     }
 
     if out_format in alias:
