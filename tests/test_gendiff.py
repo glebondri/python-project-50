21b3ab4fe3b1c18c09c61e774e29ac3e6d905e77
launc <toshadrazdov@gmail.com>
2024-02-29 21:17:02 +0300

    modified test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -2,16 +2,24 @@ from gendiff_package.gendiff import generate_diff
 
 stylish_fixture = generate_diff('tests/fixtures/nested1.json',
                                 'tests/fixtures/nested2.yml', 'stylish')
+
 plain_fixture = generate_diff('tests/fixtures/nested1.json',
                               'tests/fixtures/nested2.yml', 'plain')
 
+json_fixture = generate_diff('tests/fixtures/nested1.json',
+                             'tests/fixtures/nested2.yml', 'json')
+
 
 def test_gendiff():
-    stylish_excepted = open('tests/fixtures/output1.txt', 'r')
-    plain_excepted = open('tests/fixtures/output2.txt', 'r')
+    stylish_expected = open('tests/fixtures/output1.txt', 'r')
+    plain_expected = open('tests/fixtures/output2.txt', 'r')
+    json_expected = open('tests/fixtures/output3.txt', 'r')
 
-    assert stylish_fixture == stylish_excepted.read(), \
+    assert stylish_fixture == stylish_expected.read(), \
         'unexpected output for \'stylish\' format!'
 
-    assert plain_fixture == plain_excepted.read(), \
+    assert plain_fixture == plain_expected.read(), \
         'unexpected output for \'plain\' format!'
+
+    assert json_fixture == json_expected.read(), \
+        'unexpected output for \'json\' format!'
