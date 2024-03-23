80aba6c28d492cd79fdabcb149a0d3f00ab089ee
launc <toshadrazdov@gmail.com>
2024-03-23 19:49:35 +0300

    add type hints

--- gendiff/shared.py
+++ gendiff/shared.py
@@ -1,18 +1,21 @@
-def get_key(line):
+from typing import Any
+
+
+def get_key(line: dict) -> str:
     return line['name']
 
 
-def get_value(line):
+def get_value(line: dict) -> Any:
     return line['value']
 
 
-def get_status(line):
+def get_status(line: dict) -> str:
     return line['status']
 
 
-def get_children(line):
+def get_children(line: dict) -> list:
     return line['children']
 
 
-def is_leaf(line):
+def is_leaf(line: dict) -> bool:
     return 'value' in line
